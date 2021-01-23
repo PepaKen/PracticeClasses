5df1b3a1e4252076ef00ac31fa56ac8f66c3c4eb
PepaKen <74868751+PepaKen@users.noreply.github.com>
2021-01-23 18:17:16 +0300

    16.9.4

---
+++ Practice16.9.4.py
@@ -0,0 +1,23 @@
+class Visitor:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+
+class MoreInfo(Visitor):
+    def __init__(self, firstname, lastname, place, status):
+        super().__init__(firstname, lastname)
+        self.place = place
+        self.status = status
+
+    def getInfo(self):
+        return f"{self.firstname} {self.lastname}, {self.place}, статус: {self.status}"
+
+
+visitor_1 = MoreInfo("Иван", "Петров", "г.Москва", "Наставник")
+visitor_2 = MoreInfo("Василий", "Огарев", "г.Уфа", "Студент")
+visitor_3 = MoreInfo("Аркадий", "Дубайло", "г.Москва", "Охранник")
+
+print(f"Список гостей: \n{visitor_1.getInfo()},"
+      f"\n{visitor_2.getInfo()},"
+      f"\n{visitor_3.getInfo()}")
